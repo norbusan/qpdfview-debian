b50b367892b4fcd846bf1af8ecb46d8cab568d0a
Adam Reichold <adamreichold@myopera.com>
2012-05-29 19:01:24 +0200

    updated help file and manual page

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -37,8 +37,8 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent),
 
     // settings
 
-    DocumentView::thumbnailWidth = m_settings.value("documentView/thumbnailWidth", DocumentView::thumbnailWidth).toReal();
-    DocumentView::thumbnailHeight = m_settings.value("documentView/thumbnailHeight", DocumentView::thumbnailHeight).toReal();
+    DocumentView::thumbnailWidth = m_settings.value("documentView/thumbnailWidth", 96.0).toReal();
+    DocumentView::thumbnailHeight = m_settings.value("documentView/thumbnailHeight", 144.0).toReal();
 
     DocumentView::fitToEqualWidth = m_settings.value("documentView/fitToEqualWidth", false).toBool();
 
