22040f4cfe4a220239d111278992d17e7f6f0fa3
Adam Reichold <adamreichold@myopera.com>
2012-09-16 19:13:18 +0200

    version bump

--- sources/main.cpp
+++ sources/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
     QApplication::setOrganizationName("qpdfview");
     QApplication::setApplicationName("qpdfview");
 
-    QApplication::setApplicationVersion("0.3.3.99");
+    QApplication::setApplicationVersion("0.3.4");
 
     QApplication::setWindowIcon(QIcon(":icons/qpdfview.svg"));
 
