93f0350b2fb0153e8b43f5a96ede1e986c1fe639
Adam Reichold <adamreichold@myopera.com>
2012-11-10 22:56:54 +0100

    do not initiate a page jump if not explicitly asked for in the MainWindow

--- sources/mainwindow.h
+++ sources/mainwindow.h
@@ -50,10 +50,10 @@ public:
     QMenu* createPopupMenu();
 
 public slots:
-    bool open(const QString& filePath, int page = 1, const QRectF& highlight = QRectF());
-    bool openInNewTab(const QString& filePath, int page = 1, const QRectF& highlight = QRectF());
+    bool open(const QString& filePath, int page = -1, const QRectF& highlight = QRectF());
+    bool openInNewTab(const QString& filePath, int page = -1, const QRectF& highlight = QRectF());
 
-    bool jumpToPageOrOpenInNewTab(const QString& filePath, int page = 1, bool refreshBeforeJump = false, const QRectF& highlight = QRectF());
+    bool jumpToPageOrOpenInNewTab(const QString& filePath, int page = -1, bool refreshBeforeJump = false, const QRectF& highlight = QRectF());
 
 protected slots:
     void on_tabWidget_currentChanged(int index);
@@ -291,10 +291,10 @@ public:
     explicit MainWindowAdaptor(MainWindow* mainWindow);
 
 public slots:
-    bool open(const QString& filePath, int page = 1, const QRectF& highlight = QRectF());
-    bool openInNewTab(const QString& filePath, int page = 1, const QRectF& highlight = QRectF());
+    bool open(const QString& filePath, int page = -1, const QRectF& highlight = QRectF());
+    bool openInNewTab(const QString& filePath, int page = -1, const QRectF& highlight = QRectF());
 
-    bool jumpToPageOrOpenInNewTab(const QString& filePath, int page = 1, bool refreshBeforeJump = false, const QRectF& highlight = QRectF());
+    bool jumpToPageOrOpenInNewTab(const QString& filePath, int page = -1, bool refreshBeforeJump = false, const QRectF& highlight = QRectF());
 
     Q_NOREPLY void raiseAndActivate();
 
